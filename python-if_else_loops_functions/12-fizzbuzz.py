85d65e8d5f17d5cf60c29d171017944a4f074171
federico-paganini <perfedefer@gmail.com>
2025-05-09 22:04:37 -0300

    feat: 12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3 
+
+def fizzbuzz():
+    for i in range (1, 101):
+        words = ""
+        words += "Fizz" if i % 3 == 0 else ""
+        words += "Buzz" if i % 5 == 0 else ""
+        print(words if words else i, end=" " if i != 100 else "")
