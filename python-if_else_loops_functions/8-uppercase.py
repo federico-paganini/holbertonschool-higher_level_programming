bc0b75b2356bcdacf25bb4c6bddc377f6fa6d9b0
federico-paganini <perfedefer@gmail.com>
2025-05-09 21:08:28 -0300

    feat: 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/user/bin/python3
+
+def uppercase(str):
+    str_upper = ""
+    for char in str:
+        str_upper += chr(ord(char) - 32) if 96 < ord(char) < 123 else char
+    
+    print("{:s}".format(str_upper))
