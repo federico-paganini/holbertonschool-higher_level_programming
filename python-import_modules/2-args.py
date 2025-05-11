24ad967f9fd987b1ddb153bf246883138c1676ac
federico-paganini <perdefer@gmail.com>
2025-05-11 12:41:20 -0300

    fix(pycodestyle): New lines and indent in message

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 import sys
 
+
 def args_print():
     av = sys.argv[1:]
     ac = len(av)
 
-    message = "0 arguments." if ac == 0 else "1 argument:" if ac == 1 else f"{ac} arguments:"
+    message = (
+        "0 arguments." if ac == 0 else "1 argument:" if ac == 1
+        else f"{ac} arguments:")
     lines = ""
     lines = [f"{i}: {arg}"for i, arg in enumerate(av, start=1)]
 
     print(message + ("\n" + "\n".join(lines) if ac > 0 else ""))
+
 
 if __name__ == "__main__":
     args_print()
