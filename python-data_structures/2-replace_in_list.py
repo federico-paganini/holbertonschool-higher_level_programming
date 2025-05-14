2d1a622ac4662923411ae38ead3b6e8b6d200d66
federico-paganini <perdefer@gmail.com>
2025-05-13 23:13:12 -0300

    fix: Permissions

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx >= my_list(len) or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return my_list
     
     my_list[idx] = element
