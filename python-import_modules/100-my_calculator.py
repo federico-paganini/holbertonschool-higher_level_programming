fbeb9dcb8256e4b3dd32d5bcd48d50a21f529ac8
federico-paganini <perfedefer@gmail.com>
2025-05-14 10:38:57 -0300

    fix: Import

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
-
-def calculator():
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
-    if sys.argv[2] not in {'+', '-', '*', '/'}:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, operations(a, b, op)))
-
 
 def operations(a, b, op):
+    from calculator_1 import add, sub, mul, div
+    
     if op == '+':
         return add(a, b)
 
@@ -34,4 +17,18 @@ def operations(a, b, op):
 
 
 if __name__ == "__main__":
-    calculator()
+    import sys
+
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    if sys.argv[2] not in {'+', '-', '*', '/'}:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, operations(a, b, op)))
