43c89b4bb573988a3aab09a8609a51bd03811b70
federico-paganini <perfedefer@gmail.com>
2025-05-14 11:10:04 -0300

    feat: 4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    list_dup = my_list[:]
+    list_dup[idx] = element
+    return list_dup
