2d9aac3725800bb76436260d5159cea7e9648d06
federico-paganini <perfedefer@gmail.com>
2025-05-14 13:43:16 -0300

    fix: Condition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = 0 if len(tuple_a) == 0 else tuple_a[0]
-    a += 0 if len(tuple_b) == 0 else tuple_b[0]
-    b = 0 if len(tuple_a) == 1 else tuple_a[1]
-    b += 0 if len(tuple_b) == 1 else tuple_b[1]
-    tuple_res = a, b
-    return tuple_res
+    a = tuple_a[0] if len(tuple_a) > 0 else 0
+    a += tuple_b[0] if len(tuple_b) > 0 else 0
+    b = tuple_a[1] if len(tuple_a) > 1 else 0
+    b += tuple_b[1] if len(tuple_b) > 1 else 0
+    return (a, b)
