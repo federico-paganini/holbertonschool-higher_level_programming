73f8b6862d3b8028693e4e1efbe30ea3a3d1e860
federico-paganini <perdefer@gmail.com>
2025-05-16 23:45:54 -0300

    feat: 102-complex_delete.py

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    remove_items = list(key for key, val in a_dictionary.items() if val == value)
+    for key in remove_items:
+        a_dictionary.pop(key)
+
+    return a_dictionary
