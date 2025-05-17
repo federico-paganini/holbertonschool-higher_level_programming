2d1a3f3bca2cb22ed0891b1123d33081fc6f7df1
federico-paganini <perdefer@gmail.com>
2025-05-17 19:43:59 -0300

    fix: try, exept block change

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,13 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
+    count = 0
+    for i in range(x):
+        try:
             print(f"{my_list[i]}", end="")
             count += 1
-    except IndexError:
-        pass
+        except IndexError:
+            break
 
     print("\n")
     return count
