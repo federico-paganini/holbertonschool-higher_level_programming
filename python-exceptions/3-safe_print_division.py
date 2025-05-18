2294be1f72c795b31d940341252dcabd377a096f
federico-paganini <perdefer@gmail.com>
2025-05-17 22:41:13 -0300

    feat: 3-safe_print_division.py

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print(
+            "Inside result: {}".format(
+                f"{result:.1f}" if result is not None else "None"
+            )
+        )
+
+    return result
