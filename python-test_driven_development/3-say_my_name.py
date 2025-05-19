1730c6645d2a9b0ed308bd258a8d44e410f4142f
federico-paganini <perfedefer@gmail.com>
2025-05-18 21:08:13 -0300

    docs: Add

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+Module containing the say_my_name function.
+
+This function prints the full name given a first name and last name,
+validating that both are strings.
+
+Raises exceptions if the arguments do not pass the validations.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the full name in the format: "My name is <first_name> <last_name>".
+    """
+
     if not first_name or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
