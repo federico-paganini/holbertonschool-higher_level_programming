320c8aac72c08af7355c4e9bc786a024ed3a05e2
federico-paganini <perdefer@gmail.com>
2025-05-21 00:04:55 -0300

    fix: Tests, code refactor

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""text_indentation Module"""
 
 
 def text_indentation(text):
-    """Write a function that prints a text with 2 new \
-        lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    bool = False
+
     i = 0
     while i < len(text):
-        if bool:
-            bool = False
-            continue
-        if text[i] == "." or text[i] == ":" or text[i] == "?":
-            print(f"{text[i]}")
-            print("")
-            j = i + 1
-            count = 0
-            while j < len(text) and text[j] == " ":
-                count += 1
-                j += 1
-            i += count
+        if text[i] not in (".", "?", ":"):
+            print(text[i], end="")
+            i += 1
         else:
-            print(f"{text[i]}", end="")
-        i += 1
+            print(f"{text[i]:s}\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
