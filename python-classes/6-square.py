9437f16cbfd28efe3ac34e42002c6a3c1e04f1ac
federico-paganini <perdefer@gmail.com>
2025-05-22 21:55:09 -0300

    fix: Square 0 offset

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -96,12 +96,12 @@ class Square:
         If size is 0, an empty line is printed.
         The position is used to offset the square both vertically and horizontally.
         """
-        if self.__position[1] > 0:
-            print("\n" * self.__position[1], end="")
-
         if self.__size == 0:
             print()
             return
+
+        if self.__position[1] > 0:
+            print("\n" * self.__position[1], end="")
 
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
