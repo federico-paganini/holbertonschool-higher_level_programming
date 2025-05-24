6743cd07e14cd564949b56edf960e792488a7026
federico-paganini <perdefer@gmail.com>
2025-05-23 21:22:56 -0300

    feat: 6-max_integer.py

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -1,27 +1,18 @@
-#!/usr/bin/python3
-"""
-max_integer module.
-
-This module provides a function to find the maximum integer
-in a list of integers or floats.
-"""
+#!/bin/bash/python3
+"""Module to find the max integer in a list"""
 
 
 def max_integer(list=[]):
-    """Finds and returns the maximum integer in a list.
-
-    Args:
-        list (list): A list of integers or floats.
-
-    Returns:
-        The maximum number in the list, or None if the list is empty.
     """
-
-    if not list:
+    Function to find and return the max integer in a list of integers.
+    If the list is empty, the function returns None.
+    """
+    if len(list) == 0:
         return None
-
-    max_value = list[0]
-    for num in list[1:]:
-        if num > max_value:
-            max_value = num
-    return max_value
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
