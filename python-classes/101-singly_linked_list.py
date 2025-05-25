23a0065044be023b4098d73f22abf14f2a8df5b4
federico-paganini <perdefer@gmail.com>
2025-05-25 19:28:02 -0300

    feat: 101-singly_linked_list.py

---
+++ python-classes/101-singly_linked_list.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        result = []
+        aux = self.__head
+        while aux is not None:
+            result.append(str(aux.data))
+            aux = aux.next_node
+        return "\n".join(result)
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            aux = self.__head
+            while aux.next_node is not None and aux.next_node.data < value:
+                aux = aux.next_node
+
+            new_node.next_node = aux.next_node
+            aux.next_node = new_node
+
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
