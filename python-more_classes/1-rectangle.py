ab728d1fb382e53d9cc97f752570e89472d96575
federico-paganini <perdefer@gmail.com>
2025-05-25 13:57:14 -0300

    fix(Pycodestyle): Type comparing

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -90,7 +90,7 @@ def validate_size(value, name):
         TypeError: If value is not an integer.
         ValueError: If value is less than 0.
     """
-    if type(value) != int:
+    if type(value) is not int:
         raise TypeError(f"{name} must be an integer")
     if value < 0:
         raise ValueError(f"{name} must be >= 0")
