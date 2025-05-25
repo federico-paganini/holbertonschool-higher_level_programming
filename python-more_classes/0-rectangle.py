207e2a123953f2cb899a681f33553588519663b3
federico-paganini <perdefer@gmail.com>
2025-05-25 12:49:34 -0300

    feat: class Rectangle empty

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named Rectangle.
+
+The class is intended as a starting point to represent a rectangle,
+and will be expanded in later tasks to include attributes
+(such as size) and methods (such as area calculations, validations, etc.).
+
+Currently, it does not have any attributes or methods.
+"""
+
+
+class Rectangle:
+    """Defines an empty rectangle class."""
+
+    pass
