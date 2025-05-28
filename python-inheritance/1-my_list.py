8c6761fcbed065dc2f8bb8d63830cff4610ba031
federico-paganini <perdefer@gmail.com>
2025-05-28 20:27:58 -0300

    fix: Include word in comments

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,7 +9,7 @@ It adds a method to print the list elements in ascending sorted order.
 class MyList(list):
     """
     MyList is a subclass of the built-in list.
-    It includes an additional method to print the list sorted in ascending order,
+    It delivers an additional method to print the list sorted in ascending order,
     without modifying the original list.
     """
 
