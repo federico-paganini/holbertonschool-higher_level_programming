e4619519dfe3acf07c56500375f722eb0305531d
federico-paganini <perdefer@gmail.com>
2025-05-28 21:35:14 -0300

    fix: Line length

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,7 +9,8 @@ It adds a method to print the list elements in ascending sorted order.
 class MyList(list):
     """
     MyList is a subclass of the built-in list.
-    It delivers an additional method to print the list sorted in ascending order,
+    It delivers an additional method to print
+    the list sorted in ascending order,
     without modifying the original list.
     """
 
