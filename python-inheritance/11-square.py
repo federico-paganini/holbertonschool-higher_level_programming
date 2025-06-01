f72b47ac68eb3b1d417cfa907c719b0613919d8b
federico-paganini <perdefer@gmail.com>
2025-06-01 15:15:28 -0300

    feat: 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a Square class inheriting from Rectangle."""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square shape, inherits from Rectangle."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return f"[Square] {self.__width:d}/{self.__height:d}"
