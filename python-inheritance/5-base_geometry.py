85454f84f1e9ddede1b724c0b1d021906410fb6a
federico-paganini <perdefer@gmail.com>
2025-06-01 11:40:11 -0300

    feat: 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -12,17 +12,6 @@ class BaseGeometry:
     A base class for geometry-related operations.
 
     This class serves as a blueprint for geometric shapes.
-    It includes a placeholder method for calculating area.
     """
 
-    def area(self):
-        """
-        Calculate the area of a geometric shape.
-
-        This method is intended to be overridden in subclasses.
-        It raises an Exception to indicate that it has not been implemented.
-
-        Raises:
-            Exception: Always, with the message "area() is not implemented".
-        """
-        raise Exception("area() is not implemented")
+    pass
