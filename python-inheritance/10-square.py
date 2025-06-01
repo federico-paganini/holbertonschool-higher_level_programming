3c7b3624dfb983d7e4443cf5b4d05f11daf895e8
federico-paganini <perdefer@gmail.com>
2025-06-01 15:21:19 -0300

    fix: Code of exercise 11 deleted

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -9,6 +9,3 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
-
-    def __str__(self):
-        return f"[Square] {self._Rectangle__width:d}/{self._Rectangle__height:d}"
