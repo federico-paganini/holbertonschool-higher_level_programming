b777697fd5483dc92a6c5d1cc3da91a64136252d
federico-paganini <perdefer@gmail.com>
2025-06-01 15:21:36 -0300

    fix: Name manggling

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,5 +10,5 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
 
-    def __str__(self):
-        return f"[Square] {self.__width:d}/{self.__height:d}"
+   def __str__(self):
+        return f"[Square] {self._Rectangle__width:d}/{self._Rectangle__height:d}"
