008ce2c379c2eeed19cf286e6f499db4d78df934
federico-paganini <perdefer@gmail.com>
2025-06-01 15:11:39 -0300

    fix: Message

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,4 +7,5 @@ class Square(Rectangle):
     """Represents a square shape, inherits from Rectangle."""
 
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
