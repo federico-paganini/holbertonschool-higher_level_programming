13274e466856e42cf122a57d6459f4512a598934
federico-paganini <perfedefer@gmail.com>
2025-06-29 02:47:25 +0000

    fix: 101

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,4 @@
 -- Displays the average temperature.
-SELECT city, avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp 
+FROM temperatures GROUP BY city
+ORDER BY avg_temp DESC;
