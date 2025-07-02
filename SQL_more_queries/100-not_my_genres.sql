dd2adc1bb75069281e28fba88de3cd973a74b907
federico-paganini <perfedefer@gmail.com>
2025-07-02 00:14:28 +0000

    fix: NUmber of selects used

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,10 @@
 -- Uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
-WHERE id NOT IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    WHERE show_id = (
-        SELECT id FROM tv_shows WHERE title = 'Dexter'
-    )
-)
-ORDER BY name;
+LEFT JOIN tv_show_genres 
+     ON tv_genres.id = tv_show_genres.genre_id
+     AND tv_show_genres.show_id = (
+         SELECT id FROM tv_shows WHERE title = 'Dexter'
+     )
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_genres.name;
