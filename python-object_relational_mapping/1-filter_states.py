db9946a5199f3232098e694c8de1fae15abc76d5
federico-paganini <perfedefer@gmail.com>
2025-07-05 20:12:13 -0300

    feat: 1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all states starting with 'N' using COLLATE utf8mb4_bin"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    av = sys.argv[1:]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=av[0],
+        passwd=av[1],
+        db=av[2],
+        charset="utf8",
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' "
+        "COLLATE utf8mb4_bin ORDER BY id ASC"
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
