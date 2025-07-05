24a42917823dda58808bf2de443716062ec13a18
federico-paganini <perfedefer@gmail.com>
2025-07-05 19:58:57 -0300

    fix: Pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -24,7 +24,12 @@ if __name__ == "__main__":
     ac = len(av)
 
     conn = MySQLdb.connect(
-        host="localhost", port=3306, user=av[0], passwd=av[1], db=av[2], charset="utf8"
+        host="localhost",
+        port=3306,
+        user=av[0],
+        passwd=av[1],
+        db=av[2],
+        charset="utf8",
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
