dd9a6ffee17176cf17ade351a8e035be24197d93
federico-paganini <perfedefer@gmail.com>
2025-07-05 22:28:39 -0300

    fix: Quotes, pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -31,7 +31,9 @@ if __name__ == "__main__":
         charset="utf8",
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(av[3]))
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(av[3])
+    )
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
