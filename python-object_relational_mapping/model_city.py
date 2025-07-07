b4c299a3aabf81f3a2c628b7feec9fa99f203f70
federico-paganini <perfedefer@gmail.com>
2025-07-06 23:29:34 -0300

    fix: Pycodestyle

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -13,6 +13,8 @@ class City(Base):
 
     __tablename__ = "cities"
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement="auto")
+    id = Column(
+        Integer, primary_key=True, nullable=False, autoincrement="auto"
+    )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
