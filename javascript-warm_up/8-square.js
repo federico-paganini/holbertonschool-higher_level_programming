4a4b5b229f6dd6a035191c3e6e88309447d56358
federico-paganini <perfedefer@gmail.com>
2025-07-12 19:13:19 -0300

    feat: 8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const size = parseInt(argv[2], 10);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('#'.repeat(size))
+  }
+}
