750dc3a06641f77226c968903c40730b483a9498
federico-paganini <perfedefer@gmail.com>
2025-07-11 22:05:59 -0300

    fix: No returns need#

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,13 +4,13 @@ const argv = require('process').argv
 const av = argv.slice(2);
 
 if (av.length < 1) {
-    return console.log('No argument');
+    console.log('No argument');
 };
 
 if (av.length === 1) {
-    return console.log('Argument found');
+    console.log('Argument found');
 };
 
 if (av.length > 1) {
-    return console.log('Arguments found');
+    console.log('Arguments found');
 };
