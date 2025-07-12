c85d84d1eb3bf297731639c1b2404454fc4823fe
federico-paganini <perfedefer@gmail.com>
2025-07-11 22:43:21 -0300

    fix: Indentation

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const argv = require('process').argv;
 const times = parseInt(argv[2], 10);
 
 if (isNaN(times)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < times; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
 }
