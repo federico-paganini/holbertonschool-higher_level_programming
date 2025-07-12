05546f8e6be1075d62edb813db2e9cecd45726be
federico-paganini <perfedefer@gmail.com>
2025-07-11 22:31:40 -0300

    fix: Semistandar

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const argv = require('process').argv;
 const num = parseInt(argv[2], 10);
 
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
