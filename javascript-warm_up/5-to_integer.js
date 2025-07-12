b1ad29129968143d7de936aaf156c2ad4f4340e0
federico-paganini <perfedefer@gmail.com>
2025-07-11 22:28:28 -0300

    feat: 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+
+const num = parseInt(argv[1], 10);
+
+if (isNaN(num)) {
+    console.log("Not a number");
+} else {
+    console.log(num);
+}
