a969e7ad6ce236505b8d50b867224b9570c7a4bb
federico-paganini <perfedefer@gmail.com>
2025-07-11 22:09:01 -0300

    fix: Semistandar

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const argv = require('process').argv
+const argv = require('process').argv;
 
 const av = argv.slice(2);
 
 if (av.length < 1) {
-    console.log('No argument');
-};
+  console.log('No argument');
+}
 
 if (av.length === 1) {
-    console.log('Argument found');
-};
+  console.log('Argument found');
+}
 
 if (av.length > 1) {
-    console.log('Arguments found');
-};
+  console.log('Arguments found');
+}
