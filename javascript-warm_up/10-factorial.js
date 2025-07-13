47ede20c32a91630143ad46b2036d2062f28fdae
federico-paganini <perfedefer@gmail.com>
2025-07-13 09:34:57 -0300

    feat: 10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const num = parseInt(argv[2], 10);
+
+console.log(factorial(num));
+
+function factorial(n) {
+    if (isNaN(n) || n <= 0) return 1;
+    return n * factorial(n - 1)
+}
