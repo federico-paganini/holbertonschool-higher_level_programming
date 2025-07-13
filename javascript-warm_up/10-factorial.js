bcc7ab4c4deb5cd179f16299947f0e8298345adb
federico-paganini <perfedefer@gmail.com>
2025-07-13 09:36:24 -0300

    fix: Semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -5,6 +5,6 @@ const num = parseInt(argv[2], 10);
 console.log(factorial(num));
 
 function factorial(n) {
-    if (isNaN(n) || n <= 0) return 1;
-    return n * factorial(n - 1)
+  if (isNaN(n) || n <= 0) return 1;
+  return n * factorial(n - 1);
 }
