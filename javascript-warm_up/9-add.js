e32c47d2881387997bddd19172c8ddfa3852e61c
federico-paganini <perfedefer@gmail.com>
2025-07-12 22:25:26 -0300

    fix: Str cast to Int

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 const av = argv.slice(2);
-const num1 = av[0];
-const num2 = av[1];
 
-if (num1 === undefined || num2 === undefined) {
-
-} else {
-  console.log(add(parseInt(av[0], 10), parseInt(av[1], 10)));
-}
+console.log(add(parseInt(av[0], 10), parseInt(av[1], 10)));
 
 function add(a, b) {
   return a + b;
