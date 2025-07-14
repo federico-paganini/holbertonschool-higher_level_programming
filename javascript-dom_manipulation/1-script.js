d3a1feb66e373030057dc2a5ae2f9bfd015839e8
federico-paganini <perfedefer@gmail.com>
2025-07-14 17:36:16 -0300

    feat: 1-script.js

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+const tag_button = document.getElementById("red_header");
+tag_button.addEventListener("click", function () {
+    const header = document.querySelector("header");
+    header.style.color = "#FF0000";
+});
