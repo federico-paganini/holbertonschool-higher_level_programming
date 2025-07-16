b1c303c912a6275f1ab31d321b90533575840e2b
federico-paganini <perfedefer@gmail.com>
2025-07-15 23:28:00 -0300

    feat: 6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,17 @@
+fetch_name();
+
+async function fetch_name() {
+    const NAME_URL = "https://swapi-api.hbtn.io/api/people/5/?format=json";
+    const character_tag = document.getElementById("character");
+    try {
+        const response = await fetch(NAME_URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+
+        character_tag.textContent = data.name;
+    } catch (e) {
+        console.error("Error: ", e);
+    }
+}
