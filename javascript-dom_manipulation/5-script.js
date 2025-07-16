5c5b20bcbb94e929a725f94da508c29a74ae844c
federico-paganini <perfedefer@gmail.com>
2025-07-15 23:05:13 -0300

    feat: 5-script.js

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+const update_header = document.getElementById("update_header");
+
+update_header.addEventListener("click", () => {
+    const header_element = document.querySelector("header");
+    header_element.textContent = "New Header!!!"
+});
