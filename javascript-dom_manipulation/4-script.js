895782f801dba716f2a9b7cc92c47216486c1166
federico-paganini <perfedefer@gmail.com>
2025-07-16 10:53:55 -0300

    chore: More updated coding style

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,4 +1,4 @@
-const add_item_btn = document.getElementById("add_item");
+const add_item_btn = document.querySelector("#add_item");
 add_item_btn.addEventListener("click", () => {
     const ul_list = document.querySelector(".my_list");
     const new_element = document.createElement("li");
