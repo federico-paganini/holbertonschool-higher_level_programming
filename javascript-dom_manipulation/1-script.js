7cad7c49a053628fcd252bf299e4f286f6bfe35e
federico-paganini <perfedefer@gmail.com>
2025-07-16 10:51:55 -0300

    chore: More updated coding style

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,4 +1,4 @@
-const tag_button = document.getElementById("red_header");
+const tag_button = document.querySelector("#red_header");
 tag_button.addEventListener("click", function () {
     const header = document.querySelector("header");
     header.style.color = "#FF0000";
