208bded3ef131581543ca78113b7887456cd27af
federico-paganini <perfedefer@gmail.com>
2025-07-16 11:05:15 -0300

    feat: 7-script.js

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,21 @@
+fetch_name();
+
+async function fetch_name() {
+    const NAME_URL = "https://swapi-api.hbtn.io/api/films/?format=json";
+    const moviesList = document.querySelector("#list_movies");
+    try {
+        const response = await fetch(NAME_URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+
+        data.forEach(movie => {
+            const liMovie = document.createElement("li");
+            liMovie.textContent = movie.title;
+            moviesList.appendChild(liMovie);
+        });
+    } catch (e) {
+        console.error("Error: ", e);
+    }
+}
