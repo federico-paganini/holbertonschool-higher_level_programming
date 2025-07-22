5ebe0e9341487231158fa6e31fe77ec527e089bd
federico-paganini <perfedefer@gmail.com>
2025-07-22 17:58:06 -0300

    docs: Module and class

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Module that defines a class with restricted instance attributes."""
 class LockedClass():
+    """Class that prevents dynamic attribute creation, except 'first_name'.
+
+    The use of __slots__ restricts the instance to only allow the 'first_name'
+    attribute, saving memory and avoiding the creation of other attributes.
+    """
     __slots__ = ('first_name',)
